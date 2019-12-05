333c8e5cb04f2d8b7ca9aaf9155b5604ed9087ac
Scott <swschipke@gmail.com>
2019-12-04 18:51:01 -0700

    make username a unique key in the db

--- db/migrations/20191203135230_initial.js
+++ db/migrations/20191203135230_initial.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
   return Promise.all([
     knex.schema.createTable("users", function(table) {
       table.increments("id").primary();
-      table.string("username");
+      table.string("username").unique();
       table.string("password");
       table.timestamps(true, true);
     }),
