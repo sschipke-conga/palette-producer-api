9a9dec1aed0c8fa438bb6b438072d031b4ede92e
Scott <swschipke@gmail.com>
2019-12-04 18:41:00 -0700

    Add endpoint for loggin in a user

--- app.js
+++ app.js
@@ -10,5 +10,22 @@ app.use(cors());
 app.use(express.json());
 
 // All endpoints live here
+app.post("/api/v1/login", (request, response) => {
+  const { username, password } = request.body;
+  console.log(username, password)
+  database("users")
+    .where({ username, password })
+    .then(users => {
+      if (users.length) {
+        const { id, name, username } = users[0];
+        return response.status(200).json({ id, name, username });
+      } else {
+        return response
+          .status(401)
+          .json({error:"Username or password incorrect"});
+      }
+    })
+    .catch(err => response.status(500).json({ error: err }));
+});
 
 export default app;
