de64e0489f3102974b5507889434d54f79b78aa0
Scott <swschipke@gmail.com>
2019-12-04 18:52:51 -0700

    Add endpoint for creating a new user

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 // All endpoints live here
+
+// login an already existing user
 app.post("/api/v1/login", (request, response) => {
   const { username, password } = request.body;
   console.log(username, password)
@@ -23,6 +25,22 @@ app.post("/api/v1/login", (request, response) => {
         return response
           .status(401)
           .json({error:"Username or password incorrect"});
+      }
+    })
+    .catch(err => response.status(500).json({ error: err }));
+});
+
+// create a new user
+
+app.post("/api/v1/users", (request, response) => {
+  const { username, password } = request.body;
+  database("users")
+    .insert({ username, password }, "id")
+    .then(id => {
+      if (id.length) {
+        return response.status(201).json({ username, id: id[0] });
+      } else {
+        return response.status(422).json({ error: "Could not create user" });
       }
     })
     .catch(err => response.status(500).json({ error: err }));
